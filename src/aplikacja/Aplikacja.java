99a7a0af7759f58840f3fc8c2011eb3a700977d3
student <student@238-12.INFORMATYKA.local>
2016-11-07 09:55:29 +0100

    10:45

--- src/aplikacja/Aplikacja.java
+++ src/aplikacja/Aplikacja.java
@@ -5,12 +5,20 @@
  */
 package aplikacja;
 
+import java.awt.Insets;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -21,20 +29,33 @@ public class Aplikacja extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
+        Button btn = new Button("_TEST");
+        //btn.setText("Say 'Hello World'");
+        btn.setOnAction((event) -> {
+            System.out.println("Hello World!");
+            Platform.exit();
         });
+
+        HBox root = new HBox();
         
-        StackPane root = new StackPane();
+        Tooltip tooltip = new Tooltip("Button control");
+        Tooltip.install(btn, tooltip);
+               
+        Label lbl = new Label("Simple JavaFX application.");
+        lbl.setFont(Font.font("Serif", FontWeight.NORMAL, 20));
+        
+        Text text = new Text("ZetCode");
+        text.setFont(Font.font("Serif", FontWeight.BOLD, 76));
+        
+        text.setId("text");
+        root.setId("root");
+        
+        Scene scene = new Scene(root, 600, 200);
+        scene.getStylesheets().add("styl.css");
+        
+        root.getChildren().add(lbl);
         root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
+        root.getChildren().addAll(text);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
